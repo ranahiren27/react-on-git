b861f092e8e3d1a746a80946fef6b66baeba70e7
ranahiren27 <ranahiren27@gmail.com>
2020-09-12 03:20:47 +0530

    Updates

---
+++ precache-manifest.75a740e80112f2e2254820aa8d1137f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1010cca850b835f28a48e8f1b7f9937f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6b3ea7e443fcee72b68",
+    "url": "/static/css/main.a3c35ff5.chunk.css"
+  },
+  {
+    "revision": "91bd78f390de76e06046",
+    "url": "/static/js/2.8be362ee.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.8be362ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b3ea7e443fcee72b68",
+    "url": "/static/js/main.a12351c1.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  }
+]);
